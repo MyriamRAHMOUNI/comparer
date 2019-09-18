226a13da320592e383978a5041cccbc128968f28
Myriam Rahmouni <myriamrahmouni2016@gmail.com>
2019-09-18 04:11:26 +0200

    supprim variables

--- comparer/comparer.py
+++ comparer/comparer.py
@@ -16,7 +16,7 @@ def delta_Neq(df1, df2,nom_sortie):
 
 ## Fonction qui permet de generer le plot |Delta_Neq| en fonction du Residus 
 def plot_delta_Neq(dfd,nom_sortie):
-    pyplot.plot('residus', 'Delta_Neq_abs', data = dfd, color = 'green', linewidth = 2, marker = 'o', markersize = 2)
+    pyplot.plot('residus', 'Delta_Neq_abs', data = dfd, color = 'green', linewidth = 2)
     pyplot.xlabel('Residus')
     pyplot.ylabel('|Delta_Neq|')
     pyplot.savefig(nom_sortie+'_Delta_Neq.png')
@@ -64,7 +64,7 @@ class Data_count:
         delta_pb = df_delta_pb.sum(axis=1) 
         delta_pb.columns = ['Residus', '|Delta_PB|']    
         delta_pb.to_csv(nom_sortie+"_Delta_PB.txt", sep = " ", header = True)     
-        pyplot.plot(delta_pb, linewidth = 2, marker = 'o', markersize = 2)
+        pyplot.plot(delta_pb, linewidth = 2)
         pyplot.xlabel('Residus')
         pyplot.ylabel('|Delta_PB|')
         pyplot.savefig(nom_sortie+"_Delta_PB.png")
@@ -72,7 +72,6 @@ class Data_count:
 
         mapp=pyplot.matshow(df_delta_pb, interpolation = 'none',  aspect = 'auto')
         pyplot.xticks(range(16), ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p'])
-        pyplot.yticks(list(df_delta_pb.index),fontsize=5)
         pyplot.ylabel('Residus')
         pyplot.colorbar(mapp)
         pyplot.savefig(nom_sortie+"_MAP_delta_PB.png", bbox_inches="tight") 	
@@ -84,53 +83,49 @@ if __name__ == "__main__":
     elif len(sys.argv) != 6: 
         raise Exception('Entrer le bon nombre d arguments')
     else: 
-        print("\nInputed files:", sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4],'\n\n' "Output file:", sys.argv[5]+"_Delta_Neq.png", sys.argv[5]+"_Delta_Neq.txt", sys.argv[5]+"_Delta_PB.png", sys.argv[5]+"_Delta_PB.txt")
+        print("\nInputed files:", sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4],'\n\n' "Output file:", sys.argv[5]+"_Delta_Neq.png", sys.argv[5]+"_Delta_Neq.txt", sys.argv[5]+"_Delta_PB.png", sys.argv[5]+"_Delta_PB.txt", sys.argv[5]+"_Map_delta_PB.txt")
+        
+	    # Lecture des fichiers 
         df_Neq_1 = pd.read_csv(sys.argv[1], sep='\s+')
         df_count_1 = pd.read_csv(sys.argv[2], sep='\s+')
         df_Neq_2 = pd.read_csv(sys.argv[3], sep='\s+')
         df_count_2 = pd.read_csv(sys.argv[4], sep='\s+')
-
-        nom_sortie = str(sys.argv[5])
-        
-        nl_Neq_1 = df_Neq_1.shape[0]
-        ncl_Neq_1 = df_Neq_1.shape[1]
-        nl_count_1 = df_count_1.shape[0]
-        ncl_count_1 = df_count_1.shape[1]
-
-        nl_Neq_2 = df_Neq_2.shape[0]
-        ncl_Neq_2 = df_Neq_2.shape[1]
-        nl_count_2 = df_count_2.shape[0]
-        ncl_count_2 = df_count_2.shape[1]
-        
-        ent_count_1 = str(df_count_1.columns.values)
-        ent_count_2 = str(df_count_2.columns.values)
-        ent_Neq_1 = str(df_Neq_1.columns.values)
-        ent_Neq_2 = str(df_Neq_2.columns.values)
-
-        Neq_1 = Fichier(nl_Neq_1, ncl_Neq_1, ent_Neq_1)
-        count_1 = Fichier(nl_count_1, ncl_count_1, ent_count_1)
-        Neq_2 = Fichier(nl_Neq_2, ncl_Neq_2, ent_Neq_2)
-        count_2 = Fichier(nl_count_2, ncl_count_2, ent_count_2)    
+	
+	    # Création des objets Fichier 
+        Neq_1 = Fichier(df_Neq_1.shape[0], df_Neq_1.shape[1], str(df_Neq_1.columns.values))
+        count_1 = Fichier(df_count_1.shape[0], df_count_1.shape[1], str(df_count_1.columns.values))
+        Neq_2 = Fichier(df_Neq_2.shape[0], df_Neq_2.shape[1], str(df_Neq_2.columns.values))
+        count_2 = Fichier(df_count_2.shape[0], df_count_2.shape[1], str(df_count_2.columns.values))    
 
         print("\n-------Vérification format")
         
+        # Verification format des objets Fichier : longueur des séquences PB et entêtes 
         Neq_1.verif_sequences(Neq_2, count_1, count_2)    
-        
         Neq_1.verif_entete_Neq()
         count_1.verif_entete_count()  
         Neq_2.verif_entete_Neq() 
         count_2.verif_entete_count() 
-
-        df_delta_Neq = delta_Neq(df_Neq_1, df_Neq_2, nom_sortie)
+	
+	    #Création de la dataframe delta_Neq
+        df_delta_Neq = delta_Neq(df_Neq_1, df_Neq_2, str(sys.argv[5]))
           
-        print("\n-------Fichier "+ nom_sortie + "_Delta_Neq.txt crée")
-        plot_delta_Neq(df_delta_Neq,nom_sortie)
-        print("\n-------Figure "+ nom_sortie + "_Delta_Neq.png crée")
-       
+        print("\n-------Fichier "+ str(sys.argv[5]) + "_Delta_Neq.txt crée")
+	
+	    #Courbe delta_Neq en fonction des résidus         
+        plot_delta_Neq(df_delta_Neq, str(sys.argv[5]))
+        print("\n-------Figure "+ str(sys.argv[5]) + "_Delta_Neq.png crée")
+        
+        # Création des objets Data_count 
         data_count_1 = Data_count(df_count_1, df_count_1.iloc[3,:].sum(axis=0)) # df_count_1.iloc[3,:].sum(axis=0) == le nombre de snapshots
         data_count_2 = Data_count(df_count_2, df_count_2.iloc[3,:].sum(axis=0))
-        delta_pb = data_count_1.calcule_et_plot_delta_PB(data_count_2, nom_sortie)
-        print("\n-------Fichier "+ nom_sortie+"_matrice_Delta_PB.txt crée")
-        print("\n-------Fichier "+ nom_sortie + "_Delta_PB.png crée")
-        print("\n-------Figure " + nom_sortie + "_MAP_delta_PB.png crée")
-        print("\n-------Figure "+ nom_sortie + "_Delta_Neq.txt crée")
+        
+        # Calclule des delta PB et création des courbes et de la map         
+        data_count_1.calcule_et_plot_delta_PB(data_count_2, str(sys.argv[5]))
+       
+        print("\n-------Fichier "+ str(sys.argv[5]) +"_matrice_Delta_PB.txt crée")
+        print("\n-------Fichier "+ str(sys.argv[5]) + "_Delta_PB.png crée")
+        print("\n-------Figure " + str(sys.argv[5]) + "_MAP_delta_PB.png crée")
+        print("\n-------Figure "+ str(sys.argv[5]) + "_Delta_Neq.txt crée")
+
+
+
